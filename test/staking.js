86ba1780f75b0b506db0b7da323ed4f7e9d3978a
SStech099 <99163081+SStech099@users.noreply.github.com>
2022-02-10 20:01:16 +0530

    Delete test directory

--- test/staking.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require("chai");
-
-describe("Staking contract", function() {
-    it("Should be deployed successfully", async function() {
-        const stake = await ethers.getContractFactory("StakingRewards");
-        const stake1 = await stake.deploy();
-    })
-});
